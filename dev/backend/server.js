d875bfa3d8d33f0cc5630b20920824ae81d5f236
janamanoharan <jana.manoharan96@gmail.com>
2017-03-21 14:18:19 -0400

    setup the backend

---
+++ dev/backend/server.js
@@ -0,0 +1,28 @@
+// dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+// setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true} ));
+var server = require('http').Server(app);
+
+// // database setup
+// mongoose.connect("mongodb://localhost:27017/serverDB", function(err, db) {
+//   if(!err) {
+//     console.log("Connection successful");
+//   }
+// });
+
+var hostname = 'localhost';
+var port = 3000;
+
+//var applicationRoute = require('./router/application')(app);
+
+// listening setup
+server.listen(process.env.PORT || port, function () {
+    console.log(`Backend server running at http://${hostname}:${port}/`);
+});
